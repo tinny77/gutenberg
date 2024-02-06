73c2b831639445eba8055332397e5abe0af44b10
Jarda Snajdr <jsnajdr@gmail.com>
2024-02-06 11:18:22 +0100

    Migrate all blocks to lazyEdit

--- packages/block-library/src/list-item/index.js
+++ packages/block-library/src/list-item/index.js
@@ -6,7 +6,6 @@ import { listItem as icon } from '@wordpress/icons';
 /**
  * Internal dependencies
  */
-import lazyLoad from '../utils/lazy-load';
 import initBlock from '../utils/init-block';
 import metadata from './block.json';
 
@@ -19,9 +18,8 @@ export { metadata, name };
 
 export const settings = {
 	icon,
-	edit: lazyLoad( () =>
-		import( /* webpackChunkName: "list-item/editor" */ './edit' )
-	),
+	lazyEdit: () =>
+		import( /* webpackChunkName: "list-item/editor" */ './edit' ),
 	save,
 	merge( attributes, attributesToMerge ) {
 		return {
